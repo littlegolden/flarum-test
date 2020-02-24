73df9bd84cb8b6fcadb7fcd68db0a9cd84b0729d
skywalker <houzhenhong@hotmail.com>
2020-02-24 17:54:54 +0800

    feat: 加入QQ登录支持

---
+++ src/QqAuthController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Flarum\Auth\Qq;
+
+use Exception;
+use Flarum\Forum\Auth\Registration;
+use Flarum\Forum\Auth\ResponseFactory;
+use Flarum\Http\UrlGenerator;
+use Flarum\Settings\SettingsRepositoryInterface;
+use Laminas\Diactoros\Response\RedirectResponse;
+use League\OAuth2\Client\Provider\Qq;
+use League\OAuth2\Client\Provider\qqResourceOwner;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class QqAuthController implements RequestHandlerInterface
+{
+    /**
+     * @var ResponseFactory
+     */
+    protected $response;
+
+    /**
+     * @var SettingsRepositoryInterface
+     */
+    protected $settings;
+
+    /**
+     * @var UrlGenerator
+     */
+    protected $url;
+
+    /**
+     * @param ResponseFactory $response
+     * @param SettingsRepositoryInterface $settings
+     * @param UrlGenerator $url
+     */
+    public function __construct(ResponseFactory $response, SettingsRepositoryInterface $settings, UrlGenerator $url)
+    {
+        $this->response = $response;
+        $this->settings = $settings;
+        $this->url = $url;
+    }
+
+    /**
+     * @param Request $request
+     * @return ResponseInterface
+     * @throws Exception
+     */
+    public function handle(Request $request): ResponseInterface
+    {
+        $redirectUri = $this->url->to('forum')->route('auth.qq');
+
+        $provider = new Qq([
+            'clientId' => $this->settings->get('flarum-auth-qq.client_id'),
+            'clientSecret' => $this->settings->get('flarum-auth-qq.client_secret'),
+            'redirectUri' => $redirectUri
+        ]);
+
+        $session = $request->getAttribute('session');
+        $queryParams = $request->getQueryParams();
+
+        $code = array_get($queryParams, 'code');
+
+        if (! $code) {
+            $authUrl = $provider->getAuthorizationUrl();
+            $session->put('oauth2state', $provider->getState());
+
+            return new RedirectResponse($authUrl.'&display=popup');
+        }
+
+        $state = array_get($queryParams, 'state');
+
+        if (! $state || $state !== $session->get('oauth2state')) {
+            $session->remove('oauth2state');
+
+            throw new Exception('Invalid state');
+        }
+
+        $token = $provider->getAccessToken('authorization_code', compact('code'));
+
+        /** @var qqResourceOwner $user */
+        $user = $provider->getResourceOwner($token);
+
+        return $this->response->make(
+            'qq', $user->getOpenId(),
+            function (Registration $registration) use ($user, $provider, $token) {
+                $registration
+                    ->provideAvatar(array_get($user->toArray(), 'figureurl_qq_2') || array_get($user->toArray(), 'figureurl_qq_1'))
+                    ->suggestUsername(array_get($user->getNickname()))
+                    ->setPayload($user->toArray());
+            }
+        );
+    }
+}
